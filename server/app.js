12beab3d3f8a6f6f28c81784694801bb2f70f5dd
silgenius <olutademartin@gmail.com>
2025-09-10 13:35:45 +0100

    feat(app): Setup app server
    Update to server static files
    Fix cors issue
    create new endpoint

--- server/app.js
+++ server/app.js
@@ -1,11 +1,22 @@
-const express = require('express');
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+
+import { getVideos } from "./controllers/VideosController.js";
 
 const app = express();
 
 const port = 3000;
 
 app.use(express.json());
+app.use(cors())
+
+app.use('/videos', express.static('../videos'));
+app.use('/thumbnails', express.static('../thumbnails'));
+// console.log(path.join(__dirname, 'videos'));
+
+app.get('/videos/all', getVideos); // /videos/app?folder=default
 
 app.listen(port, () => {
-    console.log("Server running at port" + port)
+    console.log("Server running at port " + port)
 })
