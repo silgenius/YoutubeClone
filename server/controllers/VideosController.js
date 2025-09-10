00b4833524569a5f2ce00c56cd506bf0786e2278
silgenius <olutademartin@gmail.com>
2025-09-10 13:37:40 +0100

    refactor: Extract video logic into seperate module

--- server/controllers/VideosController.js
+++ server/controllers/VideosController.js
@@ -1,26 +1,6 @@
-import fs from 'fs/promises';
-import ffmpeg from 'fluent-ffmpeg';
-import path from 'path';
-
-
-// Static binary paths
-import ffmpegStatic from 'ffmpeg-static';
-import ffprobeStatic from 'ffprobe-static';
-import { stat } from 'fs';
-import { rejects } from 'assert';
-
-ffmpeg.setFfmpegPath(ffmpegStatic);
-ffmpeg.setFfprobePath(ffprobeStatic.path);
+import { videoDetails, folderPath, videoNames } from '../utils/VideoUtils.js';
 
 export async function getVideos (req, res) {
-    // const folderPath = '../videos' --> folder by default
-
+    const data = await videoDetails(folderPath, videoNames)
+    return res.status(200).json({ status: 'success', data })
 }
-
-const folderPath = '../videos'
-
-(async function execute() {
-    const videoNames = await retrieveVideos(folderPath);
-    const data = await videoDetails(folderPath, videoNames);
-    console.log(data)
-})();
